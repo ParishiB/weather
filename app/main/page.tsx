77d9109b05020c22efa1b6f798b1dec1ed0f8c4b
PARISHI BHANGE <parishibhange@PARISHIs-MacBook-Pro.local>
2024-09-01 16:21:32 +0530

    c

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -30,13 +30,54 @@
 //   );
 // }
 // app/main/page.tsx
+// "use client";
+
+// import Weather from "../components/Weather";
+// import { useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import TopSearch from "../components/TopSearch";
+// import SuspenseBoundary from "../components/SuspenseBoundary"; // Import the SuspenseBoundary component
+
+// export default function Home() {
+//   const searchParams = useSearchParams();
+//   const locality_id = searchParams.get("locality_id");
+//   const area = searchParams.get("area");
+
+//   const [favArr, setFavArr] = useState<any[]>(() => {
+//     const savedFavArr = localStorage.getItem("favArr");
+//     return savedFavArr ? JSON.parse(savedFavArr) : [];
+//   });
+
+//   useEffect(() => {
+//     localStorage.setItem("favArr", JSON.stringify(favArr));
+//   }, [favArr]);
+
+//   return (
+//     <SuspenseBoundary>
+//       <div>
+//         <Weather
+//           locality_id={locality_id}
+//           area={area}
+//           favArr={favArr}
+//           setFavArr={setFavArr}
+//         />
+//         <TopSearch />
+//       </div>
+//     </SuspenseBoundary>
+//   );
+// }
+// app/main/page.tsx
 "use client";
 
+import { Suspense } from "react";
 import Weather from "../components/Weather";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import TopSearch from "../components/TopSearch";
-import SuspenseBoundary from "../components/SuspenseBoundary"; // Import the SuspenseBoundary component
+
+const SuspenseWrapper: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
 
 export default function Home() {
   const searchParams = useSearchParams();
@@ -53,7 +94,7 @@ export default function Home() {
   }, [favArr]);
 
   return (
-    <SuspenseBoundary>
+    <SuspenseWrapper>
       <div>
         <Weather
           locality_id={locality_id}
@@ -63,6 +104,6 @@ export default function Home() {
         />
         <TopSearch />
       </div>
-    </SuspenseBoundary>
+    </SuspenseWrapper>
   );
 }
